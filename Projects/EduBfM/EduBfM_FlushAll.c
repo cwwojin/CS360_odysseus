03253c003d47f78d0a929b0267d34cc5f0e3c146
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-01 12:53:12 +0900

    Update EduBfM_FlushAll.c

--- Projects/EduBfM/EduBfM_FlushAll.c
+++ Projects/EduBfM/EduBfM_FlushAll.c
@@ -91,32 +91,26 @@ Four EduBfM_FlushAll(void)
 	type = PAGE_BUF;
 	for(i=0;i<BI_NBUFS(type);i++){
 		//iterate through all buffer elements, check if DIRTY = 1.
-		/*
 		if((BI_BITS(type, i) & DIRTY) == DIRTY){
 			//edubfm_FlushTrain(&(BI_KEY(type, i)), type);
-			//bfm_FlushTrain(&(BI_KEY(type, i)), type);
-			
-			e = RDsM_WriteTrain((BI_BUFFER(type, i)), &(BI_KEY(type, i)), BI_BUFSIZE(type));
-			if(e < 0) ERR(e);
-			bufInfo[type].bufTable[i].bits = bufInfo[type].bufTable[i].bits & ~(DIRTY);
+			bfm_FlushTrain(&(BI_KEY(type, i)), type);
 		}
-		*/
-		edubfm_FlushTrain(&(BI_KEY(type, i)), type);
 	}
 	type = LOT_LEAF_BUF;
 	for(i=0;i<BI_NBUFS(type);i++){
 		//iterate through all buffer elements, check if DIRTY = 1.
-		/*
+		
 		if((BI_BITS(type, i) & DIRTY) == DIRTY){
 			//edubfm_FlushTrain(&(BI_KEY(type, i)), type);
-			//bfm_FlushTrain(&(BI_KEY(type, i)), type);
-			
+			bfm_FlushTrain(&(BI_KEY(type, i)), type);
+			/*
 			e = RDsM_WriteTrain((BI_BUFFER(type, i)), &(BI_KEY(type, i)), BI_BUFSIZE(type));
 			if(e < 0) ERR(e);
 			bufInfo[type].bufTable[i].bits = bufInfo[type].bufTable[i].bits & ~(DIRTY);
+			*/
 		}
-		*/
-		edubfm_FlushTrain(&(BI_KEY(type, i)), type);
+		
+		//edubfm_FlushTrain(&(BI_KEY(type, i)), type);
 	}
 	/* ENDOFNEWCODE */
 	
