4352394730fdb26a78ef242e843fd7da4266f0f1
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-04 00:05:22 +0900

    Update EduBfM_FreeTrain.c

--- Projects/EduBfM/EduBfM_FreeTrain.c
+++ Projects/EduBfM/EduBfM_FreeTrain.c
@@ -96,7 +96,6 @@ Four EduBfM_FreeTrain(
 	/* NEWCODE */
 	//1.lookup key from hash table.
 	index = edubfm_LookUp(trainId, type);
-	//index = bfm_LookUp(trainId, type);
 	if(index == NOTFOUND_IN_HTABLE) ERR(eNOTFOUND_BFM);
 	btable[index].fixed--;
 	//error msg if fixed < 0.
