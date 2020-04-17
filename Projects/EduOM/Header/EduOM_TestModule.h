d70e7849dda01650be1a33b5d6aff87e905446d4
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-17 11:26:30 +0900

    Update EduOM_TestModule.h

--- Projects/EduOM/Header/EduOM_TestModule.h
+++ Projects/EduOM/Header/EduOM_TestModule.h
@@ -68,9 +68,9 @@
 /* For API function that you want to test, define it TRUE.                 */
 /* Otherwise, define it FALSE so that the solution API function is called. */
 
-#define _EDUOM_CREATEOBJECT_ 	TRUE
-#define _EDUOM_DESTROYOBJECT_ 	TRUE
-#define _EDUOM_COMPACTPAGE_		TRUE
+#define _EDUOM_CREATEOBJECT_ 	FALSE   //TRUE
+#define _EDUOM_DESTROYOBJECT_ 	FALSE   //TRUE
+#define _EDUOM_COMPACTPAGE_		FALSE   //TRUE
 #define _EDUOM_READOBJECT_		TRUE
 #define _EDUOM_NEXTOBJECT_		TRUE
 #define _EDUOM_PREVOBJECT_		TRUE
