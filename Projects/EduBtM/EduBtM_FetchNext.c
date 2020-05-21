6054fb0dbf5b0171743c081b7e90b5453e4839e5
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-05-21 22:34:25 +0900

    Update EduBtM_FetchNext.c

--- Projects/EduBtM/EduBtM_FetchNext.c
+++ Projects/EduBtM/EduBtM_FetchNext.c
@@ -242,6 +242,7 @@ Four edubtm_FetchNext(
 		next->flag = CURSOR_ON;
 		next->leaf = leaf;
 		next->slotNo = idx;
+		printf("Going to set cursor's key value : %d to new value : %d, key length is %d.\n", (Four) next->key.val[0], (Four) entry->kval[0], entry->klen);
 		memcpy(&next->key, &entry->klen, entry->klen);
 		alignedKlen = ALIGNED_LENGTH(entry->klen);
 		//next->oid = (ObjectID) entry->kval[alignedKlen];
