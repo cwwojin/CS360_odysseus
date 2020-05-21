97be5a1b6471f2adfd676f0d94ebee85e63d5a35
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-05-21 13:53:04 +0900

    Update EduBtM_DropIndex.c

--- Projects/EduBtM/EduBtM_DropIndex.c
+++ Projects/EduBtM/EduBtM_DropIndex.c
@@ -89,8 +89,8 @@ Four EduBtM_DropIndex(
 
 
     /*@ Free all pages concerned with the root. */
-	e = btm_FreePages(pFid, rootPid, dlPool, dlHead);
-	//e = edubtm_FreePages(pFid, rootPid, dlPool, dlHead);
+	//e = btm_FreePages(pFid, rootPid, dlPool, dlHead);
+	e = edubtm_FreePages(pFid, rootPid, dlPool, dlHead);
 	if(e < 0) ERR(e);
 
 	
