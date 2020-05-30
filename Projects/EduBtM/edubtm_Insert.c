52362f6f0c18314960fb7e030033f480a9f0f576
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-05-30 19:33:26 +0900

    Update edubtm_Insert.c

--- Projects/EduBtM/edubtm_Insert.c
+++ Projects/EduBtM/edubtm_Insert.c
@@ -268,7 +268,7 @@ Four edubtm_InsertLeaf(
 	alignedKlen = ALIGNED_LENGTH(kval->len);
 	entryLen = sizeof(Two) + sizeof(Two) + alignedKlen + sizeof(ObjectID);
 	//3. If (required space <= Free space)
-	//printf("Entrylen : %d, Free space : %d, FREE : %d, UNUSED : %d, nSlots : %d, @ page : %d\n", entryLen, BL_FREE(page), page->hdr.free, page->hdr.unused, page->hdr.nSlots, pid->pageNo);
+	printf("Entrylen : %d, Free space : %d, FREE : %d, UNUSED : %d, nSlots : %d, @ page : %d\n", entryLen, BL_FREE(page), page->hdr.free, page->hdr.unused, page->hdr.nSlots, pid->pageNo);
 	if(entryLen + sizeof(Two) <= BL_FREE(page)){
 		if(entryLen + sizeof(Two) > BL_CFREE(page)){	//compact page if needed.
 			//e = edubtm_CompactLeafPage(page, NIL);
